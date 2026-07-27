ab3ce966ece146cb4f1da0e1a0c22cba4b61614b
Greg Taylor <greg@gctaylor.com>
2026-07-27 22:39:50 +0200

    Add Lua configuration to tests

--- tests/lua_module_smoke.c
+++ tests/lua_module_smoke.c
@@ -1,20 +1,45 @@
 #include <lauxlib.h>
 #include <lua.h>
+#include <lualib.h>
 
+#include <stdbool.h>
 #include <stdio.h>
+
+static bool lua_configure_package_path(lua_State *state,
+                                       const char *package_directory) {
+  const char *existing_path;
+
+  lua_getglobal(state, "package");
+  if (!lua_istable(state, -1))
+    return false;
+  lua_getfield(state, -1, "path");
+  existing_path = lua_tostring(state, -1);
+  lua_pop(state, 1);
+  lua_pushfstring(state, "%s/?.lua;%s", package_directory,
+                  existing_path ? existing_path : "");
+  lua_setfield(state, -2, "path");
+  lua_pop(state, 1);
+  return true;
+}
 
 int main(int argc, char *argv[]) {
   int index;
 
-  if (argc < 2)
+  if (argc < 3)
     return 2;
 
-  for (index = 1; index < argc; index++) {
+  for (index = 2; index < argc; index++) {
     lua_State *state = luaL_newstate();
     int result;
 
     if (!state)
       return 2;
+    luaL_openlibs(state);
+    if (!lua_configure_package_path(state, argv[1])) {
+      fprintf(stderr, "unable to configure Lua package path\n");
+      lua_close(state);
+      return 2;
+    }
     result = luaL_loadfile(state, argv[index]);
     if (!result)
       result = lua_pcall(state, 0, 1, 0);
